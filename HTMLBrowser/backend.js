eaf9a1c6af4757544d7f5035803eceaa51dcc1c1
SpencerU12994 <spencerunderhill@southdevon.ac.uk>
2024-11-19 09:21:32 +0000

    Initial File Commit

---
+++ HTMLBrowser/backend.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const path = require('path')
+
+
+
+//import fs from 'fs';
+//import path from 'path';
+//import process from 'process';
+
+
+
+var moveFrom = "../";
+
+// Loop through all the files in the temp directory
+fs.readdir(moveFrom, function (err, files) {
+  if (err) {
+    console.error("Could not list the directory.", err);
+    process.exit(1);
+  }
+
+  files.forEach(function (file, index) {
+    // Make one pass and make the file complete
+    var fromPath = path.join(moveFrom, file);
+
+    fs.stat(fromPath, function (error, stat) {
+      if (error) {
+        console.error("Error stating file.", error);
+        return;
+      }
+
+      if (stat.isFile()) console.log("'%s' is a file.", fromPath);
+      else if (stat.isDirectory())
+        console.log("'%s' is a directory.", fromPath);
+    });
+  });
+});
